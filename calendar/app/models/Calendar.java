3193e469d3ac306f84b2a478f01898347997fecf
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2011-10-03 14:57:46 +0200

    Refactoring

--- calendar/app/models/Calendar.java
+++ calendar/app/models/Calendar.java
@@ -93,7 +93,6 @@ public class Calendar implements Iterable<Event> {
             	}
         	}
     	}
-    	
 	}
 	
 }
--- calendar/test/UserTest.java
+++ calendar/test/UserTest.java
@@ -1,7 +1,3 @@
-
-
-
-
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
